6ce5a6dd8fabf56cff35cd80f189f011762661f4
KirawzWeb <95690980+KirawzWeb@users.noreply.github.com>
2025-04-01 00:04:34 +0200

    Update thabronxx3.lua

--- thabronxx3.lua
+++ thabronxx3.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("Get new script in discord.gg/ciagov if you buy this script you get scammed.")
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/64060c710b62c12704c07c906f0fd454.lua"))()
